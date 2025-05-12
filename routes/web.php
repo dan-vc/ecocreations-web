4eefd64e9f055b2f55843e2ea83b67cc8308b328
Daniel Villafranqui <danedo20472@gmail.com>
2025-05-12 18:24:27 -0500

    Ruta Nosotros añnadida

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-<<<<<<< HEAD
     return view('inicio');
-=======
+});
+
+Route::get('/nosotros', function () {
     return view('nosotros');
->>>>>>> Junior
 });
