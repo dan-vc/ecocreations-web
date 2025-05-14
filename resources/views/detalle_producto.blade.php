2dde18d28b406a443de78e65bd8ea6d10b7c0672
Junior Ynga <141764815+wizzarlive@users.noreply.github.com>
2025-05-14 16:32:15 -0500

    Se adapto el codigo en responsive

--- resources/views/detalle_producto.blade.php
+++ resources/views/detalle_producto.blade.php
@@ -17,7 +17,7 @@
         </div>
 
         <div class="card-2">
-            <p class="text_1">Contenedor Hermético Cuadrado 500ml</p>
+            <p class="text_1">CONTENEDOR HERMÉTICO CUADRADO 500ML</p>
             <p class="text_2">S/13.00</p>
             <p class="text_3">Cantidad:</p>
             <div class="card-1-1">
@@ -39,12 +39,14 @@
         <hr>
     </div>
     <div class="block-2">
-        <p class="text_1">PRODUCTOS RECOMENDADOS</p>
+        <div class="texto">
+            <p class="text_1">PRODUCTOS RECOMENDADOS</p>
+        </div>
         <div class="card-1">
             <div class="card-1-1">
                 <div>
-                    <img src="{{ asset('img/detalle_producto/producto ejemplo.png') }}" alt="Foto desde Laravel"
-                        width="235" height="232">
+                    <img src="{{ asset('img/detalle_producto/termos.png') }}" alt="Foto desde Laravel" width="235"
+                        height="232">
                     <p class="text_1">TERMOS</p>
                     <p class="text_2">termo que aguanta el calor por 14 horas en calor</p>
                     <p class="text_3">S/20.70</p>
@@ -54,8 +56,8 @@
 
             <div class="card-1-1">
                 <div>
-                    <img src="{{ asset('img/detalle_producto/producto ejemplo.png') }}" alt="Foto desde Laravel"
-                        width="235" height="232">
+                    <img src="{{ asset('img/detalle_producto/termos.png') }}" alt="Foto desde Laravel" width="235"
+                        height="232">
                     <p class="text_1">TERMOS</p>
                     <p class="text_2">termo que aguanta el calor por 14 horas en calor</p>
                     <p class="text_3">S/20.70</p>
@@ -65,8 +67,8 @@
 
             <div class="card-1-1">
                 <div>
-                    <img src="{{ asset('img/detalle_producto/producto ejemplo.png') }}" alt="Foto desde Laravel"
-                        width="235" height="232">
+                    <img src="{{ asset('img/detalle_producto/termos.png') }}" alt="Foto desde Laravel" width="235"
+                        height="232">
                     <p class="text_1">TERMOS</p>
                     <p class="text_2">termo que aguanta el calor por 14 horas en calor</p>
                     <p class="text_3">S/20.70</p>
@@ -82,35 +84,36 @@
     </div>
 
     <div class="block-3">
-        <div>
-            <div>
-                <img src="{{ asset('img/detalle_producto/producto ejemplo.png') }}" alt="Foto desde Laravel" width="179"
-                    height="173">
-                <p>CONFIANZA</p>
-                <p>Transparencia en el origen y proceso de nuestros productos reciclados.</p>
-            </div>
 
-            <div>
-                <img src="{{ asset('img/detalle_producto/producto ejemplo.png') }}" alt="Foto desde Laravel" width="179"
-                    height="173">
-                <p>CONFIANZA</p>
-                <p>Transparencia en el origen y proceso de nuestros productos reciclados.</p>
-            </div>
-
-            <div>
-                <img src="{{ asset('img/detalle_producto/producto ejemplo.png') }}" alt="Foto desde Laravel" width="179"
-                    height="173">
-                <p>CONFIANZA</p>
-                <p>Transparencia en el origen y proceso de nuestros productos reciclados.</p>
-            </div>
-
-            <div>
-                <img src="{{ asset('img/detalle_producto/producto ejemplo.png') }}" alt="Foto desde Laravel" width="179"
-                    height="173">
-                <p>CONFIANZA</p>
-                <p>Transparencia en el origen y proceso de nuestros productos reciclados.</p>
-            </div>
+        <div class="card-1">
+            <img src="{{ asset('img/detalle_producto/confianza.png') }}" alt="Foto desde Laravel" width="179"
+                height="173">
+            <p class="text_1">CONFIANZA</p>
+            <p class="text_2">Transparencia en el origen y proceso de nuestros productos reciclados.</p>
         </div>
+
+        <div class="card-1">
+            <img src="{{ asset('img/detalle_producto/pago en linea.png') }}" alt="Foto desde Laravel" width="179"
+                height="173">
+            <p class="text_1">PAGO EN LÍNEA</p>
+            <p class="text_2">Comodidad y seguridad en tus transacciones.</p>
+        </div>
+
+        <div class="card-1">
+            <img src="{{ asset('img/detalle_producto/puntualidad.png') }}" alt="Foto desde Laravel" width="179"
+                height="173">
+            <p class="text_1">PUNTUALIDAD</p>
+            <p class="text_2">Apoya la economía circular y el cuidado del planeta.</p>
+        </div>
+
+        <div class="card-1">
+            <img src="{{ asset('img/detalle_producto/tienda.png') }}" alt="Foto desde Laravel" width="179"
+                height="173">
+            <p class="text_1">RECOJO EN TIENDA</p>
+            <p class="text_2"> Dale un toque original y consciente a tu día a día.</p>
+        </div>
+
+
     </div>
 
 </body>
