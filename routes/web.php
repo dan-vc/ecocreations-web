4bd2814d8560b1e07c2f638f2a556a6310aaf7c1
Daniel Villafranqui <danedo20472@gmail.com>
2025-05-18 20:22:13 -0500

    Merge branch Junior_dos

--- routes/web.php
+++ routes/web.php
@@ -6,15 +6,10 @@ Route::get('/', function () {
     return view('inicio');
 });
 
-<<<<<<< HEAD
 Route::get('nosotros', function () {
     return view('nosotros');
 });
 
 Route::get('contacto', function () {
     return view('contacto');
-=======
-Route::get('/detalle',function(){
-    return view('detalle_producto');
->>>>>>> Junior_dos
 });
